83da5530fdf486ce61e58fdba3b30c03b4bcf2b0
mu88 <4560672+mu88@users.noreply.github.com>
2024-08-03 10:07:08 +0200

    test: introduce new testing project

---
+++ tests/DummyAspNetCoreProject/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.MapGet("/hello", () => "World")
+   .WithOpenApi();
+
+await app.RunAsync();
