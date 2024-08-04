381040f1abec6886827ec6b7a881da1b15d54136
mu88 <4560672+mu88@users.noreply.github.com>
2024-08-04 16:21:15 +0200

    docs: add API doc comments

--- src/mu88.Shared/OpenTelemetry/HostApplicationBuilderExtensions.cs
+++ src/mu88.Shared/OpenTelemetry/HostApplicationBuilderExtensions.cs
@@ -8,9 +8,34 @@ using OpenTelemetry.Trace;
 
 namespace mu88.Shared.OpenTelemetry;
 
+/// <summary>
+///     Extensions for an <see cref="IHostApplicationBuilder" /> instance.
+/// </summary>
 // ReSharper disable once UnusedType.Global - reviewed mu88: public API
 public static class HostApplicationBuilderExtensions
 {
+    /// <summary>
+    ///     Adds and configures the following OpenTelemetry features:
+    ///     <list type="bullet">
+    ///         <item>
+    ///             <description>Metrics for ASP.NET Core, .NET process and .NET runtime.</description>
+    ///         </item>
+    ///         <item>
+    ///             <description>Traces for ASP.NET Core and Entity Framework Core.</description>
+    ///         </item>
+    ///     </list>
+    /// </summary>
+    /// <param name="builder">
+    ///     The <see cref="IHostApplicationBuilder" /> instance on which the OpenTelemetry features will be
+    ///     configured.
+    /// </param>
+    /// <param name="serviceName">The name of the service so that it can be identified (e.g. the application name).</param>
+    /// <param name="logger">An optional logger.</param>
+    /// <returns>The provided <paramref name="builder" /> with configured OpenTelemetry features.</returns>
+    /// <remarks>
+    ///     Don't forget to set the .NET configuration parameter <c>OTEL_EXPORTER_OTLP_ENDPOINT</c> for the OpenTelemetry
+    ///     endpoint receiving the exported metrics and traces.
+    /// </remarks>
     // ReSharper disable once UnusedMember.Global - reviewed mu88: public API
     public static IHostApplicationBuilder ConfigureOpenTelemetry(this IHostApplicationBuilder builder, string serviceName, ILogger? logger = null)
     {
