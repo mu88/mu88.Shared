fc67c05427f905ea8a6789eb9715abce9a523f7f
mu88 <4560672+mu88@users.noreply.github.com>
2024-10-05 17:07:18 +0200

    test: add test for multi-arch Docker image publishing

---
+++ tests/Tests/Integration/BuildTargetTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using FluentAssertions;
+
+namespace Tests.Integration;
+
+[TestFixture]
+[Category("Integration")]
+public class BuildTargetTests
+{
+    [Test]
+    public void MultiArchPublish_ShouldPublishMultiArchImage()
+    {
+        // Arrange
+        var processStartInfo = new ProcessStartInfo
+        {
+            FileName = "dotnet", Arguments = $"publish {GetTestProjectFile()} /t:MultiArchPublish", RedirectStandardOutput = true, UseShellExecute = false
+        };
+
+        // Act
+        var process = Process.Start(processStartInfo);
+        var output = process!.StandardOutput.ReadToEnd();
+        process.WaitForExit();
+
+        // Assert
+        Console.WriteLine(output);
+        output.Should().Contain("Created and pushed manifest list registry.hub.docker.com/mu88/mu88-shared-dummy:dev");
+    }
+
+    private static string GetTestProjectFile() => Path.Combine(new DirectoryInfo(Environment.CurrentDirectory).Parent!.Parent!.Parent!.Parent!.FullName,
+        "DummyAspNetCoreProject",
+        "DummyAspNetCoreProject.csproj");
+}
