6972a644af81d6975e85adb1896a2a5af15acb54
mu88 <4560672+mu88@users.noreply.github.com>
2025-12-19 12:43:09 +0100

    feat: publish fully qualified image via MSBuild

--- tests/Tests/Unit/SharedTargetsTests.cs
+++ tests/Tests/Unit/SharedTargetsTests.cs
@@ -16,63 +16,66 @@ public class SharedTargetsTests
             new object[]
             {
                 "At least one of PublishRegularContainer or PublishChiseledContainer must be 'true'",
-                new Dictionary<string, string>(StringComparer.Ordinal)
-            },
-            new object[]
-            {
-                "At least one of PublishRegularContainer or PublishChiseledContainer must be 'true'",
-                new Dictionary<string, string>(StringComparer.Ordinal)
-                    { { "PublishChiseledContainer", "false" }, { "PublishRegularContainer", "false" } }
+                new Dictionary<string, string>(StringComparer.Ordinal) { { "PublishChiseledContainer", "false" }, { "PublishRegularContainer", "false" } }
             },
             new object[]
             {
                 "PublishRegularContainer property must be 'true' or 'false'",
-                new Dictionary<string, string>(StringComparer.Ordinal)
-                    { { "PublishChiseledContainer", "true" }, { "PublishRegularContainer", "bla" } }
+                new Dictionary<string, string>(StringComparer.Ordinal) { { "PublishChiseledContainer", "true" }, { "PublishRegularContainer", "bla" } }
             },
             new object[]
             {
                 "PublishChiseledContainer property must be 'true' or 'false'",
-                new Dictionary<string, string>(StringComparer.Ordinal)
-                    { { "PublishChiseledContainer", "bla" }, { "PublishRegularContainer", "true" } }
+                new Dictionary<string, string>(StringComparer.Ordinal) { { "PublishChiseledContainer", "bla" }, { "PublishRegularContainer", "true" } }
             },
             new object[]
             {
                 "IsRelease property must be set ('true' or 'false')",
                 new Dictionary<string, string>(StringComparer.Ordinal)
-                    { { "PublishChiseledContainer", "true" }, { "IsRelease", string.Empty }, { "ReleaseVersion", "dev" } }
+                {
+                    { "PublishChiseledContainer", "true" }, { "IsRelease", string.Empty }, { "ReleaseVersion", "dev" }
+                }
             },
             new object[]
             {
                 "IsRelease property must be set ('true' or 'false')",
                 new Dictionary<string, string>(StringComparer.Ordinal)
-                    { { "PublishRegularContainer", "true" }, { "IsRelease", string.Empty }, { "ReleaseVersion", "dev" } }
+                {
+                    { "PublishRegularContainer", "true" }, { "IsRelease", string.Empty }, { "ReleaseVersion", "dev" }
+                }
             },
             new object[]
             {
                 "ReleaseVersion property must be set (e.g., '1.2.3')",
                 new Dictionary<string, string>(StringComparer.Ordinal)
-                    { { "PublishChiseledContainer", "true" }, { "IsRelease", "true" }, { "ReleaseVersion", string.Empty } }
+                {
+                    { "PublishChiseledContainer", "true" }, { "IsRelease", "true" }, { "ReleaseVersion", string.Empty }
+                }
             },
             new object[]
             {
                 "ReleaseVersion property must be set (e.g., '1.2.3')",
                 new Dictionary<string, string>(StringComparer.Ordinal)
-                    { { "PublishRegularContainer", "true" }, { "IsRelease", "true" }, { "ReleaseVersion", string.Empty } }
+                {
+                    { "PublishRegularContainer", "true" }, { "IsRelease", "true" }, { "ReleaseVersion", string.Empty }
+                }
             }
         ];
     }
 
     [TestCaseSource(nameof(Cases_PublishContainersForMultipleFamilies_ShouldFail_WhenPropertiesAreInvalidOrMissing))]
     public async Task PublishContainersForMultipleFamilies_ShouldFail_WhenPropertiesAreInvalidOrMissing(string expectedErrorMessage,
-        [SuppressMessage("Design", "MA0016:Prefer using collection abstraction instead of implementation", Justification = "Limitation of NUnit")]
-        Dictionary<string, string> buildParameters)
+                                                                                                        [SuppressMessage("Design",
+                                                                                                            "MA0016:Prefer using collection abstraction instead of implementation",
+                                                                                                            Justification = "Limitation of NUnit")]
+                                                                                                        Dictionary<string, string> buildParameters)
     {
         // Arrange
         var rootDirectory = Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.Parent ?? throw new NullReferenceException();
         var testProjectFile = Path.Join(rootDirectory.FullName, "DummyAspNetCoreProject", "DummyAspNetCoreProject.csproj");
         buildParameters.Add("DryRun", "true");
-        var arguments = $"publish {testProjectFile} /t:PublishContainersForMultipleFamilies {string.Join(' ', buildParameters.Select(kvp => $"-p:{kvp.Key}=\"{kvp.Value}\""))}";
+        var arguments =
+            $"publish {testProjectFile} /t:PublishContainersForMultipleFamilies /p:DoNotApplyGitHubScope=true {string.Join(' ', buildParameters.Select(kvp => $"-p:{kvp.Key}=\"{kvp.Value}\""))}";
 
         // Act
         var outputLines = await Helper.WaitUntilToolFinishedAsync("dotnet", arguments, false, CancellationToken.None);
@@ -87,13 +90,13 @@ public class SharedTargetsTests
         // Arrange
         var rootDirectory = Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.Parent ?? throw new NullReferenceException();
         var testProjectFile = Path.Join(rootDirectory.FullName, "DummyAspNetCoreProject", "DummyAspNetCoreProject.csproj");
-        var arguments = $"msbuild {testProjectFile} /t:PrecomputeContainerRepository --getProperty:ComputedContainerRepository";
+        var arguments = $"msbuild {testProjectFile} /t:PrecomputePublishingProperties /p:DoNotApplyGitHubScope=true --getProperty:ComputedContainerRepository";
 
         // Act
         var outputLines = await Helper.WaitUntilToolFinishedAsync("dotnet", arguments, true, CancellationToken.None);
 
         // Assert
-        outputLines.Should().HaveCount(1).And.ContainMatch("mu88/mu88-shared-dummy");
+        outputLines.Should().HaveCount(1).And.ContainMatch("dummyaspnetcoreproject");
     }
 
     [Test]
@@ -102,12 +105,76 @@ public class SharedTargetsTests
         // Arrange
         var rootDirectory = Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.Parent ?? throw new NullReferenceException();
         var testProjectFile = Path.Join(rootDirectory.FullName, "DummyAspNetCoreProject", "DummyAspNetCoreProject.csproj");
-        var arguments = $"msbuild {testProjectFile} /t:PrecomputeContainerRepository /p:ContainerRepository=\"me/test\" --getProperty:ComputedContainerRepository";
+        var arguments =
+            $"msbuild {testProjectFile} /t:PrecomputePublishingProperties /p:DoNotApplyGitHubScope=true /p:ContainerRepository=\"me/test\" --getProperty:ComputedContainerRepository";
 
         // Act
         var outputLines = await Helper.WaitUntilToolFinishedAsync("dotnet", arguments, true, CancellationToken.None);
 
         // Assert
         outputLines.Should().HaveCount(1).And.ContainMatch("me/test");
+    }
+
+    [Test]
+    public async Task PublishContainerForMultipleFamilies_ShouldPrecomputeFullyQualifiedImageWithoutRegistry_WhenNotSet()
+    {
+        // Arrange
+        var rootDirectory = Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.Parent ?? throw new NullReferenceException();
+        var testProjectFile = Path.Join(rootDirectory.FullName, "DummyAspNetCoreProject", "DummyAspNetCoreProject.csproj");
+        var arguments = $"msbuild {testProjectFile} /t:PrecomputePublishingProperties /p:DoNotApplyGitHubScope=true --getProperty:ComputedFullyQualifiedImage";
+
+        // Act
+        var outputLines = await Helper.WaitUntilToolFinishedAsync("dotnet", arguments, true, CancellationToken.None);
+
+        // Assert
+        outputLines.Should().HaveCount(1).And.ContainMatch("dummyaspnetcoreproject");
+    }
+
+    [Test]
+    public async Task PublishContainerForMultipleFamilies_ShouldPrecomputeFullyQualifiedImageWithRegistry_WhenNotSet()
+    {
+        // Arrange
+        var rootDirectory = Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.Parent ?? throw new NullReferenceException();
+        var testProjectFile = Path.Join(rootDirectory.FullName, "DummyAspNetCoreProject", "DummyAspNetCoreProject.csproj");
+        var arguments =
+            $"msbuild {testProjectFile} /t:PrecomputePublishingProperties /p:DoNotApplyGitHubScope=true /p:ContainerRegistry=\"ghcr.io\" --getProperty:ComputedFullyQualifiedImage";
+
+        // Act
+        var outputLines = await Helper.WaitUntilToolFinishedAsync("dotnet", arguments, true, CancellationToken.None);
+
+        // Assert
+        outputLines.Should().HaveCount(1).And.ContainMatch("ghcr.io/dummyaspnetcoreproject");
+    }
+
+    [Test]
+    public async Task PublishContainerForMultipleFamilies_ShouldUseSpecifiedContainerRepositoryWithoutRegistry_WhenSet()
+    {
+        // Arrange
+        var rootDirectory = Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.Parent ?? throw new NullReferenceException();
+        var testProjectFile = Path.Join(rootDirectory.FullName, "DummyAspNetCoreProject", "DummyAspNetCoreProject.csproj");
+        var arguments =
+            $"msbuild {testProjectFile} /t:PrecomputePublishingProperties /p:DoNotApplyGitHubScope=true /p:ContainerRepository=\"me/test\" --getProperty:ComputedFullyQualifiedImage";
+
+        // Act
+        var outputLines = await Helper.WaitUntilToolFinishedAsync("dotnet", arguments, true, CancellationToken.None);
+
+        // Assert
+        outputLines.Should().HaveCount(1).And.ContainMatch("me/test");
+    }
+
+    [Test]
+    public async Task PublishContainerForMultipleFamilies_ShouldUseSpecifiedContainerRepositoryWithRegistry_WhenSet()
+    {
+        // Arrange
+        var rootDirectory = Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.Parent ?? throw new NullReferenceException();
+        var testProjectFile = Path.Join(rootDirectory.FullName, "DummyAspNetCoreProject", "DummyAspNetCoreProject.csproj");
+        var arguments =
+            $"msbuild {testProjectFile} /t:PrecomputePublishingProperties /p:DoNotApplyGitHubScope=true /p:ContainerRegistry=\"ghcr.io\" /p:ContainerRepository=\"me/test\" --getProperty:ComputedFullyQualifiedImage";
+
+        // Act
+        var outputLines = await Helper.WaitUntilToolFinishedAsync("dotnet", arguments, true, CancellationToken.None);
+
+        // Assert
+        outputLines.Should().HaveCount(1).And.ContainMatch("ghcr.io/me/test");
     }
 }
